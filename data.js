be2858587f800ac922d2ff8798c0e5a1f0598e8a
Miguel Fernandez <miguel.fernandez@codeimmersives.com>
2019-04-01 14:31:07 -0400

    Added education

--- data.js
+++ data.js
@@ -1,11 +1,23 @@
 const education = [
     {
         address: '630 Ninth Ave, Ste 901, New York, NY 10036',
-        degree: 'Web Development',
+        degree: 'Web Development Certificate',
         school: 'Code Immersives',
         year: 'January 2019 - December 2019'
-        
+    },
+    {
+        address: 'Cincinnati, Ohio',
+        degree: 'Bachelor of Science in Clinical Laboratory Science',
+        school: 'University of Cincinnati',    
+        year: '2006-2011'
+    },
+    {
+        address: 'Williamstown, Massachusetts',
+        degree: 'Bachelor of Economics',
+        school: 'Williams College',
+        year: 'September 1997 - June 2001'
     }
+
 ]
 
 const experience = [
