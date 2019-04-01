92c76fe0b4a05e6d50d19290cee9b0635b1327e3
Miguel Fernandez <miguel.fernandez@codeimmersives.com>
2019-04-01 14:27:02 -0400

    Added experiences to data

---
+++ data.js
@@ -0,0 +1,37 @@
+const education = [
+    {
+        address: '630 Ninth Ave, Ste 901, New York, NY 10036',
+        degree: 'Web Development',
+        school: 'Code Immersives',
+        year: 'January 2019 - December 2019'
+        
+    }
+]
+
+const experience = [
+    {
+        employer: 'New York Presbyterian Hospital',
+        title: 'Blood Bank',
+        year: 'September 2014 - Present'
+    },
+    {
+        employer: 'North Shore University Hospital',
+        title: 'Blood Bank',
+        year: 'February 2012 - August 2014'
+    },
+    {
+        employer: 'Northwell Health - Core Laboratory',
+        title: 'Laboratory Technician - Microbiology',
+        year: 'January 2010 - February 2012'
+    },
+    {
+        employer: 'U.S. Army - USARIEM',
+        title: 'Research Technician - Mountain Medicinew',
+        year: 'September 2006 - June 2009'
+    },
+    {
+        employer: 'U.S. Army - USAMRIID',
+        title: 'Research Technician - Bacteriology Division',
+        year: 'March 2003 - September 2006'
+    }
+]
